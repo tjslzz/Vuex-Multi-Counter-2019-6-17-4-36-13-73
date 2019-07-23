817b05f6bcc71847eba94e5e649334c6624b9cd5
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-23 11:44:56 +0800

    [jerryLi] refactor : rm unused codes

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,20 +1,9 @@
 <template>
-<h3 id="counter">请输入数字 <input type="text" v-model.number="count" @input="update"></h3>
+<h3 id="counter">请输入数字 <input type="text" v-model.number="$store.state.number"></h3>
 </template>
 
 <script>
 export default {
-    name: 'counter',
-    data() {
-        return {
-            count: ''
-        }
-    },
-    methods: {
-        update() {
-            this.$store.commit('update', this.count);
-        }
-    }
-
+    name: 'counter'
 }
 </script>
